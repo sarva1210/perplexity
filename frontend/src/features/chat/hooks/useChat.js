423577df6d4c93e77bd150fcd4473bb103f6d874
sarva1210 <sarva.vish77@gmail.com>
2026-03-20 17:28:35 +0530

    updated chats

--- frontend/src/features/chat/hooks/useChat.js
+++ frontend/src/features/chat/hooks/useChat.js
@@ -8,19 +8,22 @@ export const useChat = () => {
 
     async function handleSendMessage({ message, chatId }) {
         dispatch(setLoading(true))
+
         const data = await sendMessage({ message, chatId })
         const { chat, aiMessage } = data
-        dispatch(createNewChat({
-            chatId: chat._id,
-            title: chat.title,
-        }))
+
+        if(!chatId)
+            dispatch(createNewChat({
+                chatId: chat._id,
+                title: chat.title,
+            }))
         dispatch(addNewMessage({
-            chatId: chat._id,
+            chatId: chatId || chat._id,
             content: message,
             role: "user",
         }))
         dispatch(addNewMessage({
-            chatId: chat._id,
+            chatId: chatId || chat._id,
             content: aiMessage.content,
             role: aiMessage.role,
         }))
@@ -29,8 +32,10 @@ export const useChat = () => {
 
     async function handleGetChats() {
         dispatch(setLoading(true))
+        
         const data = await getChats()
         const { chats } = data
+
         dispatch(setChats(chats.reduce((acc, chat) => {
             acc[ chat._id ] = {
                 id: chat._id,
@@ -43,18 +48,23 @@ export const useChat = () => {
         dispatch(setLoading(false))
     }
 
-    async function handleOpenChat(chatId) {
-        const data = await getMessages(chatId)
-        const { messages } = data
+    async function handleOpenChat(chatId, chats) {
+        console.log(chats[ chatId ]?.messages.length)
 
-        const formattedMessages = messages.map(msg => ({
-            content: msg.content,
-            role: msg.role,
-        }))
-        dispatch(addMessages({
-            chatId,
-            messages: formattedMessages,
-        }))
+        if (chats[ chatId ]?.messages.length === 0) {
+            const data = await getMessages(chatId)
+            const { messages } = data
+
+            const formattedMessages = messages.map(msg => ({
+                content: msg.content,
+                role: msg.role,
+            }))
+
+            dispatch(addMessages({
+                chatId,
+                messages: formattedMessages,
+            }))
+        }
         dispatch(setCurrentChatId(chatId))
     }
 
